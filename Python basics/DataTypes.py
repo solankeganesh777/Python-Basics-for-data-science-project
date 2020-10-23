237cc847ad152306eaf575cdf1a3cc9823c2bdbe
Ganesh Solanke <solankeganesh777@gmail.com>
2020-10-23 10:23:20 +0530

    Update DataTypes.py

--- Python basics/DataTypes.py
+++ Python basics/DataTypes.py
@@ -1,7 +1,18 @@
 # Check the Python Version
 
+"""
+Text Type: 	str
+Numeric Types: 	int, float, complex
+Sequence Types: 	list, tuple, range
+Mapping Type: 	dict
+Set Types: 	set, frozenset
+Boolean Type: 	bool
+Binary Types: 	bytes, bytearray, memoryview
+"""
+
 import sys
 print(sys.version)
+
 
 #Type check
 print(type(32))
@@ -35,4 +46,32 @@ type(False)
 bool(0)
 float(True)
 
-
+#Complex
+x=1j
+print(x)
+print(type(x))
+           
+#Sequence: Range
+x=range(6)
+print(x)  
+print(type(x) 
+ 
+#Set:FrozenSet
+y=frozenset({'apple','mango','melon'})
+print(y)      
+print(type(y))      
+      
+#Binary:bytes 
+x=b"hello"
+print(x)
+print(type(x))
+      
+#Binary:bytearray
+x=bytearray(5) 
+print(x)
+print(x)
+      
+#Binary:Memoryview
+x=memoryview(bytes(5))
+print(x)
+print(type(x)      
