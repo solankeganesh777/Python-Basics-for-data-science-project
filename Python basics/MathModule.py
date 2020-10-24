9a85fa5bea777635dc5f899ecc060c10996e13c1
Ganesh Solanke <solankeganesh777@gmail.com>
2020-10-24 13:14:01 +0530

    Create MathModule.py

---
+++ Python basics/MathModule.py
@@ -0,0 +1,26 @@
+#Math function
+
+#Max,Min
+lis=[1,5,9,5,66,74,555]
+print(max(lis))
+print(min(lis))
+
+print("\n")
+#Absolute(positive) value
+print(abs(-7.8))
+
+print("\n")
+#power
+print(pow(3,3))
+
+print("\n")
+
+import math
+
+print(dir(math))
+
+print(math.sqrt(64))
+print(math.ceil(1.2))
+print(math.ceil(1.9))
+print(math.pi)
+print(math.factorial(4))
