61d8eee3bd83e14d3f612fc048a67f724f93223e
Ganesh Solanke <solankeganesh777@gmail.com>
2020-11-26 20:06:05 +0530

    Create ArraySorting.py

---
+++ Numpy/ArraySorting.py
@@ -0,0 +1,20 @@
+# Numpy Sorting Arrays
+
+x=np.array([55,5,3,8,2,44,45])
+y=np.sort(x)
+print(x)
+print(y)
+print(type(y))
+
+print("\n")
+
+# Sort array of strings alphabetically
+li=['ganesh','mangesh','vaishali','balasaheb']
+y=np.sort(li)
+print(y)
+
+print('\n')
+
+# Sort 2-D array
+arr = np.array([[3, 2, 4], [5, 0, 1],[5,16,8]])
+print(np.sort(arr))
