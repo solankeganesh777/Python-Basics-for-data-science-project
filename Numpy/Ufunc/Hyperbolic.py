54dddfb319195d68bfc8a277968e6222e3d6a58e
Ganesh Solanke <solankeganesh777@gmail.com>
2020-11-28 16:47:07 +0530

    Create Hyperbolic.py

---
+++ Numpy/Ufunc/Hyperbolic.py
@@ -0,0 +1,22 @@
+# Hyperbolic functions: sinh(),cosh(),tanh()
+
+print(np.sinh(np.pi/2))
+print(np.cosh(np.pi/2))
+print(np.tanh(np.pi/2))
+
+x=np.array([np.pi/2,np.pi/4,np.pi/6])
+print(np.sinh(x))
+print(np.cosh(x))
+print(np.tanh(x))
+
+print("\n")
+# finding angles
+
+print(np.arcsinh(1.0))
+print(np.arccosh(2.0))
+print(np.arctanh(0.3))
+
+x=np.array([0.1,0.2,0.5])
+print(np.arcsinh(x))
+print(np.arccosh(x))
+print(np.arctanh(x))
